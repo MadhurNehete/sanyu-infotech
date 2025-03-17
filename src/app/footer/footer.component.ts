689b59925b85480554a6028b0575a9e59cbecc60
MansiTemkar <mansitemkar2003@gmail.com>
2025-03-17 15:28:39 +0530

    1 commit

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FooterService } from '../services/footer.service';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+  constructor(public footerService: FooterService) {}
+
+  toggleFooter() {
+    this.footerService.toggleFooter();
+  }
+}
+
